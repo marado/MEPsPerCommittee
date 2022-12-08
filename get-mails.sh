b7e8d0426eb62f1a4852a94c5bc275c0fcf0c948
Marcos Marado <mindboosternoori@gmail.com>
2022-12-08 00:00:44 +0000

    make get-mails.sh great again (or working at all)
    Website updates mean that scrappers stop working. This revamp of the
    script doesn't change it functionally, only makes it working again.
    Things are a bit simplified now, as each commitee page lists all meps
    in one page, instead of having it paginated like before.

--- get-mails.sh
+++ get-mails.sh
@@ -15,6 +15,7 @@ COM=(
   "budg"
   "cont"
   "econ"
+  "fisc"
   "empl"
   "envi"
   "itre"
@@ -29,9 +30,13 @@ COM=(
   "afco"
   "femm"
   "peti"
-  "terr"
-  "pest"
-  "tax3"
+  "beca"
+  "covi"
+  "ing2"
+  "inge"
+  "aida"
+  "anit"
+  "pega"
 )
 
 PS3="Which committee do you want the emails from? "
@@ -41,47 +46,22 @@ do
     for i in ${!COM[@]}
     do
       if [ "${COM[i]}" = "$choice" ]; then
-		# change this to the URL of the committee you want to get e-mail addresses for
-		url="http://www.europarl.europa.eu/committees/en/${COM[i]}/members.html?action="
-
 		# Let's do this:
 		DEBUG=false
-		rm -rf debug *html
 
-		# There's always at least one page:
-		wget $url"0" -o /dev/null -O "0.html";
+		# change this to the URL of the committee you want to get e-mail addresses for
+		wget "https://www.europarl.europa.eu/committees/en/${COM[i]}/home/members" -o /dev/null -O - |grep mepphoto|cut -d/ -f5|cut -d. -f1 > meps-urls
 
-		# number of pages with members there are. I should be extracting this, but whatever
-		pages=$(grep \"?action= 0.html|grep class=\"\" |grep -v next_page|tail -1|cut -d\" -f2|cut -d= -f2);
-		if [ "$DEBUG" != true ]; then
-		  echo "There are $pages pages to process"
-		fi
-
-		# get the pages
-		for p in $(seq 1 $pages); do
-			wget "$url$p" -o /dev/null -O "$p.html";
-		done
-
-		# get the MEP page URLs
-		grep photo_mep *html|cut -d\" -f2|sed 's/\ /%20/g' > meps-urls
-
-		# get the MEP pages
-		if [ "$DEBUG" != true ]; then
-			rm *html
-		else
-			mkdir debug; mv *html debug
-		fi
 		for m in $(cat meps-urls); do
-			wget "http://www.europarl.europa.eu$m" -o /dev/null;
+			wget "https://www.europarl.europa.eu/meps/en/$m" -o /dev/null -O "$m.html";
 		done
 
 		# get the email addresses from the MEP pages
-		grep mailto *html|cut -d\" -f4|cut -d: -f2-|rev|sed 's/\]ta\[/@/g'|sed 's/\]tod\[/\./g' > emails
+		grep link_email\  ./*html|cut -d\" -f4|rev|sed 's/\]ta\[/@/g'|sed 's/\]tod\[/\./g' > emails
 
 		# cleanup
 		if [ "$DEBUG" != true ]; then
-			rm meps-urls
-			rm *html
+			rm meps-urls ./*html
 		fi
         break
       fi
